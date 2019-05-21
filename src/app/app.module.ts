7f3ab95c4447c3d507a0da2a759a0ba819a8e379
Paul Halliday <hello@paulhalliday.io>
2019-05-21 15:38:26 +0100

    Added HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { FormsModule } from '@angular/forms';
 
@@ -7,20 +8,17 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { ShoppingReducer } from './store/reducers/shopping.reducer';
 
-import { ShoppingItemComponent } from './shopping-item/shopping-item.component';
-import { ShoppingFormComponent } from './shopping-form/shopping-form.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingItemComponent,
-    ShoppingFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     StoreModule.forRoot({
       shopping: ShoppingReducer
     }),
